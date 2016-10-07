c5b078a340d91c886077e163e07e7a5e648fd93b
Francisco Huertas <francisco@fhuertas.com>
2016-10-07 12:58:27 +0200

    Added tests for the mocks

--- tests/mock_decorators/module_test.py
+++ tests/mock_decorators/module_test.py
@@ -1,2 +1,19 @@
 def function_sum(param_a, param_b):
     return param_a + param_b
+
+
+a_parameter = 10
+
+
+class TestClass:
+    mocked = False
+
+    def function_echo(self, *args, **kwargs):
+        return str(*args)
+
+    def function(self):
+        return False
+
+    @classmethod
+    def class_function(cls):
+        return False
