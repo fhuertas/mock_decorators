b3d58f415bfef86a46de708a8272db123e0ac433
Francisco Huertas <francisco@fhuertas.com>
2016-11-03 17:17:50 +0100

    Add incompatibility to python 2 for ClassMock

--- bin/tests.sh
+++ bin/tests.sh
@@ -24,7 +24,7 @@ fi
 
 
 export COVERAGE_FILE=.coverage
-$TEST_DIR/bin/nosetests --verbosity=3 --with-coverage --cover-erase --cover-package=mock_decorators tests
+$TEST_DIR/bin/nosetests --verbosity=3 --with-coverage --cover-package=mock_decorators tests
 $TEST_DIR/bin/coverage xml
 echo "Total report"
 $TEST_DIR/bin/coverage report
